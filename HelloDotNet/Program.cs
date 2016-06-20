f320aa4cd0813b8baaa66a6b452b2d4c179c7c7c
Ian Ceicys <ian.ceicys@autodesk.com>
2016-06-20 17:36:29 -0400

    Adding Key and .Track

--- HelloDotNet/Program.cs
+++ HelloDotNet/Program.cs
@@ -20,11 +20,18 @@ namespace HelloDotNet
                 .AndCustomAttribute("groups", "beta_testers")
                 .AndCustomAttribute("favoritenumber", 42);
 
+            //Custom configuration object:
+            Configuration config = LaunchDarkly.Client.Configuration.Default()
+            .WithApiKey("sdk-51cd9173-cc65-4345-af2e-0c31c7d4afe9")
+            .WithEventQueueFrequency(TimeSpan.FromSeconds(2));
+            LdClient ldClient = new LdClient(config);
+
             // TODO: Enter the key for your feature flag key here
             var value = client.Toggle("new-wizbang-feature", user, false);
 
             if (value)
             {
+                ldClient.Track("new-wizbang-feature", user,"Feature Enabled");
                 Console.WriteLine("Showing feature for user " + user.Key);
             }
             else
